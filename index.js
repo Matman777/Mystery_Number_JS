eefe17fbc2079df70d52c196c9c1f3ca20da3b31
Matman777 <mathias.mauer777@gmail.com>
2024-01-19 14:24:40 +0100

    "try to push"

---
+++ index.js
@@ -0,0 +1,5 @@
+import { prompt } from "./prompt.js";
+
+// const test = prompt("ceci est un test");
+
+const question = prompt("Devinez le nombre mystère: ");
---
+++ prompt.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export function prompt(question) {
+  // readlineSync.question returns the user's input once they hit enter
+  const answer = readlineSync.question(question);
+
+  return answer;
+}
