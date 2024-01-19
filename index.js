c8dad7a351d813cb6c21aba8f8dadf9a80eece12
Matman777 <mathias.mauer777@gmail.com>
2024-01-19 16:50:22 +0100

    reload ok

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 import { prompt } from "./prompt.js";
 
 
-let userChoice;
-let attempts = 0;
-const targetNumber = Math.floor(Math.random()*100+1);
-console.log(targetNumber);
+let reloadGame = true;
 
-while (userChoice !== targetNumber){
+do {
 
-    const userChoice = Number(prompt("Guess the mystery number: "));
-    attempts++;
+    let targetNumber = Math.floor(Math.random()*100+1);
+    console.log(targetNumber);
+    let attempts = 0;
+    let userChoice;
 
-    if (userChoice === "" || isNaN(userChoice) || userChoice < 0 || userChoice > 100){
-        console.log("Enter a valid number between 0 and 100");
-        continue;
-    }else if (userChoice > targetNumber) {
-        console.log("Target number is smaller");
-    }else if (userChoice < targetNumber) {
-        console.log("Target number is bigger");
-    }else{
-        console.log(`You found it in ${attempts} attempts! Yes, it was ${targetNumber}!`)
-        break
-    }
-    
-}
+        while (userChoice !== targetNumber){
+
+            userChoice = Number(prompt("Guess the mystery number: "));
+            attempts++;
+
+            if (userChoice === "" || isNaN(userChoice) || userChoice < 0 || userChoice > 100){
+                console.log("Enter a valid number between 0 and 100");
+                continue;
+            }else if (userChoice > targetNumber) {
+                console.log("Target number is smaller");
+            }else if (userChoice < targetNumber) {
+                console.log("Target number is bigger");
+            }else{
+                console.log(`Yes, it was ${targetNumber}! You found it in ${attempts} attempts!`);
+            }
+                
+        }
+        let validResponse = false;
+            while (!validResponse) {
+                reloadGame = prompt("Another game? Enter 'y' for Yes and 'n' for No: ").toLowerCase();
+
+                if (reloadGame === "y" || reloadGame === "n") {
+                    validResponse = true;
+                } else {
+                    console.log("Please enter 'y' for Yes or 'n' for No.");
+                }
+            }
+    } while (reloadGame === "y");
 
