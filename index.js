bd45577564e01a31f7a6b97555e63aa66719d5fc
Matman777 <mathias.mauer777@gmail.com>
2024-01-19 15:06:26 +0100

    logic ok

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import { prompt } from "./prompt.js";
 
-// const test = prompt("ceci est un test");
 
-const question = prompt("Guess the mystery number: ");
+let userChoice;
+let attempts = 0;
+const targetNumber = Math.floor(Math.random()*100+1);
+console.log(targetNumber);
 
-const mysteryNumber = Math.floor(Math.random()*100+1);
-console.log(mysteryNumber);
+while (userChoice !== targetNumber){
+
+    const userChoice = Number(prompt("Guess the mystery number: "));
+    attempts++;
+
+    if (userChoice === "" || isNaN(userChoice) || userChoice < 0 || userChoice > 100){
+        console.log("Enter a valid number between 0 and 100");
+        continue;
+    }else if (userChoice > targetNumber) {
+        console.log("Target number is smaller");
+    }else if (userChoice < targetNumber) {
+        console.log("Target number is bigger");
+    }else{
+        console.log(`You found it in ${attempts} attempts! Yes, it was ${targetNumber}!`)
+        break
+    }
+    
+}
 
